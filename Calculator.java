ecbc978a43bb47e369fe454ecc7c6d2aafdca214
Maksim Pozdeev <maximglazov18@gmail.com>
2018-11-09 00:30:16 +0300

    add: -*/, choice operations

--- Calculator.java
+++ Calculator.java
@@ -10,7 +10,7 @@ public class Calculator{
 	
 	/**
 		Суммируем аргументы.
-		@param params Аргументы суммирования
+		@param params Аргументы сожения (addition)
 	*/
 	public void add(int ... params){
 		for (Integer param: params){
@@ -18,6 +18,37 @@ public class Calculator{
 		}
 	}
 	
+	/**
+		Вычитаем аргументы.
+		@param params Аргументы разность (subtraction)
+	*/
+	public void sub(int ... params){
+		for (Integer param: params){
+			this.result -= param;
+		}
+	}
+	
+	/**
+		Умножаем аргументы.
+		@param params Аргументы умнодения (multiplication)
+	*/
+	public void mul(int ... params){
+		for (Integer param: params){			
+			this.result *= param;			
+		}
+	}
+	
+	/**
+		Делим аргументы.
+		@param params Аргументы деления (division)
+	*/
+	public void div(int ... params){
+		for (Integer param: params){
+			this.result /= param;
+		}
+	}
+	
+		
 	/**
 		Получить результат.
 		@return результат вычисления
--- InteractRunner.java
+++ InteractRunner.java
@@ -12,13 +12,49 @@ public class InteractRunner{
 			Calculator calc = new Calculator();
 			String exit = "no";
 			while(!exit.equals("yes")){
-				System.out.println("Enter first arg: ");
+				System.out.println("Result =  " + calc.getResult());
+				System.out.print("Clear Result? y/n  ");
+				String res = reader.next();
+				if (res.equals("y")){
+					calc.cleanResult();
+				}
+				
+				System.out.print("Enter first arg: ");
 				String first = reader.next();
-				System.out.println("Enter second arg: ");
+				System.out.print("Enter second arg: ");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first)+Integer.valueOf(second));
-				System.out.println("Result: " + calc.getResult());
-				calc.cleanResult();
+				System.out.print("Enter operations (+ - * / Clear): ");
+				String operation = reader.next();
+				
+				if (operation.equals("+")){
+					calc.add(Integer.valueOf(first), Integer.valueOf(second));
+					System.out.println("Result: " + calc.getResult());
+				}
+				
+				if (operation.equals("-")){
+					calc.sub(Integer.valueOf(first), Integer.valueOf(second));
+					System.out.println("Result: " + calc.getResult());
+				}
+				
+				if (operation.equals("*")){
+					calc.mul(Integer.valueOf(first), Integer.valueOf(second));
+					System.out.println("Result: " + calc.getResult());
+				}
+				
+				if (operation.equals("/")){
+					if (Integer.valueOf(second) == 0) {
+						System.out.println("Деление на ноль запрещено!");
+					}else {
+						calc.div(Integer.valueOf(first), Integer.valueOf(second));
+						System.out.println("Result: " + calc.getResult());
+					}
+				}
+				
+				if (operation.equals("Clear")){
+					calc.cleanResult();
+					System.out.println("Result: " + calc.getResult());
+				}
+				
 				System.out.println("Exit: yes/no ");
 				exit = reader.next();
 			}
